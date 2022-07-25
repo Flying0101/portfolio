9fb3ddb8d07bcb07f68b82085e42d6c8e3553af6
Flying0101 <stephano@live.se>
2022-07-26 00:55:11 +0200

    footer-logic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+import { FaPhone } from 'react-icons/fa';
 
 import '../css/Footer.css';
 
@@ -10,21 +12,30 @@ function Footer() {
 
 
 
-
-
-
-
-
-
-
-
   return (
-    <div className="footer">
-      <ul>
-        <li className="fContact">contact</li>
-      </ul>
+    <footer className="footer">
+      <div className="fotGrid">
+        <div className="fotDiv">
+          <h3 className="fotH">Get in touch</h3>
 
-    </div>
+          <ul className="fotList">
+            <li className="fotLi"><FaMapMarkerAlt className="icons" /> Sweden/Stockholm</li>
+            <li className="fotLi"><FaEnvelope className="icons" /> Stephano@live.se</li>
+            <li className="fotLi"><FaPhone className="icons" /> 0734123125</li>
+
+          </ul>
+
+        </div>
+
+
+        <div className="fotDiv2">
+          <h3 className="fotH">About</h3>
+          <p className="aboutP">A focused developer always looking to strive and evovle with the team, aiming for new heights with each projects.</p>
+
+
+        </div>
+      </div>
+    </footer>
   );
 }
 
